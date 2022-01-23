e34d3169eff4b09f935c90c679f13cd1199d7b71
Damilare Durojaye <damilare.durojaye@nordsec.com>
2022-01-23 14:46:24 +0100

    Used package and public functions (Just capitalize the function name)

--- main.go
+++ main.go
@@ -1,56 +1,28 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"math"
+	"github.com/babadee08/go-functions/simplemath"
 )
 
 func main() {
-	fmt.Printf("%f\n", add(2, 6))
-	fmt.Printf("%f\n", subtract(2, 6))
-	fmt.Printf("%f\n", multiply(2, 6))
+	fmt.Printf("%f\n", simplemath.Add(2, 6))
+	fmt.Printf("%f\n", simplemath.Subtract(2, 6))
+	fmt.Printf("%f\n", simplemath.Multiply(2, 6))
 
-	answer, err := divide(6, 3)
+	answer, err := simplemath.Divide(6, 3)
 	if err != nil {
 		fmt.Printf("An error occured %s\n", err.Error())
 	} else {
 		fmt.Printf("%f\n", answer)
 	}
 	// ignoring the error
-	answer, _ = divide(6, 0)
+	answer, _ = simplemath.Divide(6, 0)
 	fmt.Printf("%f\n", answer)
 
 	//total := sum(12.2, 14, 16, 22.4)
 	numbers := []float64{12.2, 14, 16, 22.4}
-	total := sum(numbers...)
+	total := simplemath.Sum(numbers...)
 	fmt.Printf("total of sum: %f\n", total)
 
-}
-
-func sum(values ...float64) float64 {
-	total := 0.0
-	for _, value := range values {
-		total += value
-	}
-	return total
-}
-
-func divide(p1, p2 float64) (float64, error) {
-	if p2 == 0 {
-		return math.NaN(), errors.New("cannot divide by zero")
-	}
-	return p1 / p2, nil
-}
-
-func add(p1, p2 float64) float64 {
-	return p1 + p2
-}
-
-func subtract(p1, p2 float64) float64 {
-	return p1 - p2
-}
-
-func multiply(p1, p2 float64) float64 {
-	return p1 * p2
 }
